71671d42287105065e001ea0b3f2b728c4e11e7c
joxanna <joxannaxue@gmail.com>
2024-11-29 12:43:48 +1100

    port update

--- backend/index.js
+++ backend/index.js
@@ -26,5 +26,5 @@ app.get('/', (req, res) => {
 
 // start server code
 app.listenport,async()=>{
-  console.log("Server is running on port number:", port)
+  console.log("Server is running on port number:", port);
 };
